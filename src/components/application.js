d1b0431176638f54d9afca1102338c65aa8736e6
Igor <igorexat@gmail.com>
2018-05-25 16:45:19 +0300

    Initial code of react application

--- src/components/application.js
+++ src/components/application.js
@@ -1,6 +1,29 @@
-import React, { Component, Children } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component, Children } from 'react';
+import PropTypes from 'prop-types';
+import {} from 'mwa';
 
 export class Application extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.app = props.app;
+    }
 
+    getChildContext() {
+        return { app: this.app }
+    }
+
+    render() {
+        return Children.only(this.props.children)
+    }
 }
+
+Application.propTypes = {
+    app: IocPropTypes.iocResolverType.isRequired,
+    children: PropTypes.element.isRequired
+};
+
+Application.childContextTypes = {
+    app: IocPropTypes.iocResolverType.isRequired
+};
+
+
--- src/modules/react-render.module.js
+++ src/modules/react-render.module.js
@@ -1,3 +1,75 @@
-/**
- * Created by itretyakov on 8/25/17.
- */
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Application } from '../components';
+
+export class ReactRenderModule {
+    static _wrappers = null;
+    static _ownWrappersCreated = false;
+
+    /**
+     * Registers new component wrapper
+     * @param {Function} wrapper
+     */
+    static registerWrapper(wrapper) {
+        /** To prevent changes in array of parent wrappers in case inheritance */
+        if (!this._ownWrappersCreated) {
+            /** Parent wrappers will be resolved on call of method getWrappers*/
+            this._wrappers = [];
+            this._ownWrappersCreated = true;
+        }
+
+        this._wrappers.push(wrapper);
+    }
+
+    static getWrappers() {
+        const ownWrappers = this._wrappers || [];
+
+        if (typeof(super.getWrappers) === 'function') {
+            return super.getWrappers().concat(ownWrappers)
+        }
+
+        return  ownWrappers;
+    }
+
+    static registerWrappers(...args) {
+        for (let wrapper of args) {
+            this.registerWrapper(wrapper);
+        }
+    }
+
+    constructor(app) {
+        this.app = app;
+        this.ioc = null;
+    }
+    configure(ioc) {
+        this.ioc = ioc;
+    }
+    run() {
+        ReactDOM.render(
+            (
+                <Application app={module.app}>
+                    {this._renderWrapperElement()}
+                </Application>
+            ),
+            this.getContainer(),
+            this.renderCallback.bind(this)
+        );
+    }
+
+    _renderWrapperElement() {
+        this.constructor.getWrappers().reverse().reduce(
+            (element, wrapper) => wrapper(element, this),
+            this.render()
+        )
+    }
+
+    render() {
+        throw new Error(`Method ${this.constructor.name}.render is not implemented!`);
+    }
+
+    getContainer() {
+        throw new Error(`Method ${this.constructor.name}.getContainer is not implemented!`);
+    }
+
+    renderCallback() {}
+}
